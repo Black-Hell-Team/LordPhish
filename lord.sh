79f4087090450428d05de785739ea22c23508194
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-05-17 14:30:35 -0400

    update lord.sh

--- lord.sh
+++ lord.sh
@@ -79,7 +79,7 @@ user_interrupt(){
         sleep 2
         printf "\e[0m\n"
         printf " \e[0m\e[1;42m Thanks for Using This Tool !!\e[0m  \e[1;44>"
-        printf " \e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m]\e[0m\e[1;96m Visit github.com/grenoxx \e[0m"
+        printf " \e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m]\e[0m\e[1;96m Visit github.com/Ch4r0nN \e[0m"
         printf "\e[0m\n"
         exit 1
 
